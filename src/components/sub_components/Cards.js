409e68cfe35d5926a74769e888ab58d4a33b5976
Blazekla <cristlui4@gmail.com>
2020-02-11 08:44:32 -0600

    added spacing code to project cards

--- src/components/sub_components/Cards.js
+++ src/components/sub_components/Cards.js
@@ -11,13 +11,18 @@ import {
 } from "@material-ui/core";
 import Link from "@material-ui/core/Link";
 
-// import seal from "./sealpup.jpg";
 const useStyles = makeStyles({
   card: {
     transition: "all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1)",
     "&:hover": {
       transform: "translateY(-5px)"
     }
+  },
+  cardAction: {
+    justifyContent: "space-between"
+  },
+  cardContent: {
+    textAlign: "center"
   }
 });
 
@@ -32,12 +37,12 @@ function ContentCard(props) {
           image="./sealpup.jpg"
           title="Photogenic Seal Pup"
         />
-        <CardContent>
+        <CardContent className={classes.cardContent}>
           <Typography color="secondary">{props.title}</Typography>
         </CardContent>
       </CardActionArea>
 
-      <CardActions>
+      <CardActions className={classes.cardAction}>
         <Button size="small" color="primary">
           <Link color="textPrimary" href={props.code} target="_blank">
             View Code
