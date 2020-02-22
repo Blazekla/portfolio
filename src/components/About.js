897e4630d781cd60c5fcbb5e5591b720b7b04a0f
Luis Cristo <cristlui4@gmail.com>
2020-02-22 00:28:55 -0600

    added more content to the About component

--- src/components/About.js
+++ src/components/About.js
@@ -31,15 +31,23 @@ function About() {
           paddingTop: "70px"
         }}
       >
-        <Typography variant="h4" color="secondary">
+        <Typography
+          variant="h4"
+          color="secondary"
+          style={{ marginBottom: "20px" }}
+        >
           About Me
         </Typography>
-
-        <Typography style={{ color: color }}>
-          Here are some technologies I've been using:
+        <Typography paragraph>
+          Ever since I came across computers as a kid, I've been mesmerized by
+          their potential. I've always found myself taking opportunites to learn
+          more about computers by tinkering and building with them. I have
+          decided to take this passion and turn it into a career.
         </Typography>
 
-        <Typography style={{ color: color }}>
+        <Typography>Here are some technologies I've been using:</Typography>
+
+        <Typography>
           React, ES6+, HTML, CSS, Material-UI, Redux, Axios, Netlify, Node.js,
           Express.
         </Typography>
