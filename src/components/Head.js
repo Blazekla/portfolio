a2062044743f1c3b110d7bf263137ea314e0cdd0
Luis Cristo <cristlui4@gmail.com>
2020-03-12 16:35:03 -0500

    added og and twitter seo without image file

--- src/components/Head.js
+++ src/components/Head.js
@@ -16,6 +16,30 @@ function Head() {
       <meta itemProp="description" content={pageData.head.siteDescription} />
       {/* <meta itemProp="image" content={}/> */}
 
+      <meta property="og:title" content={pageData.head.title} />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content="" />
+      <meta property="og:url" content={pageData.head.siteUrl} />
+      <meta property="og:description" content={pageData.head.description} />
+      <meta property="og:site_name" content={pageData.head.title} />
+
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta property="og:image:type" content="image/png" />
+      <meta property="og:locale" content="en_US" />
+
+      {/* Twitter */}
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={pageData.head.title} />
+      <meta name="twitter:description" content={pageData.head.description} />
+      <meta name="twitter:url" content={pageData.head.siteUrl} />
+      <meta name="twitter:image" content="" />
+
+      <meta name="twitter:site" content="" />
+      <meta name="twitter:creator" content="" />
+      <meta name="twitter:image:alt" content={pageData.head.title} />
+
       <meta name="theme-color" content="#546686" />
     </Helmet>
   );
